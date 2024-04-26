ea57d032fd15534e95e1df6158a9906d27d204c5
DeLeon-JianPaolo-BSIT32E2 <161408072+Sagre-HazelJoy-BSIT32E2@users.noreply.github.com>
2024-04-26 14:45:29 +0800

    Update on MainActivity with comments

--- app/src/main/java/com/midtermexam/DeLeonTracker/MainActivity.kt
+++ app/src/main/java/com/midtermexam/DeLeonTracker/MainActivity.kt
@@ -21,72 +21,58 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
+// Declare MainActivity class, inheriting from AppCompatActivity
 class MainActivity : AppCompatActivity() {
+    // listdata objects
+    // Declare lateinit variables for view binding, toolbar, list, edit texts, buttons, search view, and date TextView
     lateinit var binding:ActivityMainBinding
     lateinit var toolbar: MaterialToolbar
     var listcat= arrayListOf<listdata>()
-    lateinit var input:EditText
-    lateinit var input2:EditText
-    lateinit var pickDateBtn:Button
-    lateinit var date:TextView
-    private lateinit var fileHelper: Filehelper
-    lateinit var delete1:Button
-    lateinit var searchView: SearchView
-
-
-
-
+    lateinit var input:EditText // Declare an uninitialized EditText variable for input
+    lateinit var input2:EditText // Declare an uninitialized EditText variable for input2
+    lateinit var pickDateBtn:Button // Declare an uninitialized Button variable for pickDateBtn
+    lateinit var date:TextView // Declare an uninitialized TextView variable for date
+    private lateinit var fileHelper: Filehelper // Declare a private lateinit variable for fileHelper of type Filehelper
+    lateinit var delete1:Button // Declare an uninitialized Button variable for delete1
+    lateinit var searchView: SearchView // Declare an uninitialized SearchView variable for searchView
 
     @RequiresApi(Build.VERSION_CODES.N)
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        fileHelper = Filehelper(this)
-        listcat = fileHelper.getTask()
-        binding= ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        input=findViewById(R.id.input1)
-        input2=findViewById(R.id.input2)
-        toolbar=findViewById(R.id.toolbar)
-        delete1=findViewById(R.id.button3)
-        searchView=findViewById(R.id.search)
+    override fun onCreate(savedInstanceState: Bundle?) { // Override the onCreate method
+        super.onCreate(savedInstanceState) // Call the superclass onCreate method with the saved instance state
+        setContentView(R.layout.activity_main) // Set the content view to the activity_main layout
+        fileHelper = Filehelper(this) // Initialize the fileHelper variable with a new instance of Filehelper class passing this activity context
+        listcat = fileHelper.getTask() // Initialize the listcat variable with the tasks retrieved from fileHelper
+        binding= ActivityMainBinding.inflate(layoutInflater) // Inflate the view using view binding
+        setContentView(binding.root) // Set the content view to the root view of the inflated layout
+        input=findViewById(R.id.input1) // Initialize the input variable with the EditText input1 from the layout
+        input2=findViewById(R.id.input2) // Initialize the input2 variable with the EditText input2 from the layout
+        toolbar=findViewById(R.id.toolbar) // Initialize the toolbar variable with the MaterialToolbar from the layout
+        delete1=findViewById(R.id.button3) // Initialize the delete1 variable with the Button button3 from the layout
+        searchView=findViewById(R.id.search) // Initialize the searchView variable with the SearchView search from the layout
 
+        var itdate:String // Declare a variable for storing the selected date as a string
 
-
-//        listcat.add(listdata("Tanmoy","666"))
-//        listcat.add(listdata("Rahul","667"))
-//        listcat.add(listdata("Sumit","668"))
-//        listcat.add(listdata("Hament","669"))
-//        listcat.add(listdata("Hament","669"))
-//        listcat.add(listdata("Hament","669"))
-//        listcat.add(listdata("Hament","669"))
-//        listcat.add(listdata("Hament","669"))
-//        listcat.add(listdata("Hament","669"))
-
-
-        var itdate:String
-
-        pickDateBtn = findViewById(R.id.button2)
-        date = findViewById(R.id.textView6)
+        pickDateBtn = findViewById(R.id.button2) // Initialize the pickDateBtn variable with the Button button2 from the layout
+        date = findViewById(R.id.textView6) // Initialize the date variable with the TextView textView6 from the layout
         var dateit:String=""
         var count:Int=0
 
         pickDateBtn.setOnClickListener {
-            count++
-            val c = Calendar.getInstance()
+            count++ // Increment the count variable
+            val c = Calendar.getInstance() // Get an instance of Calendar
 
-            val year = c.get(Calendar.YEAR)
-            val month = c.get(Calendar.MONTH)
-            val day = c.get(Calendar.DAY_OF_MONTH)
+            val year = c.get(Calendar.YEAR) // Get the current year
+            val month = c.get(Calendar.MONTH) // Get the current month
+            val day = c.get(Calendar.DAY_OF_MONTH) // Get the current day
 
-            val datePickerDialog = DatePickerDialog(
+            val datePickerDialog = DatePickerDialog( // Create a new DatePickerDialog
                 this,
                 { view, year, monthOfYear, dayOfMonth ->
-
+                    // Update the date TextView with the selected date
                     date.text =(dayOfMonth.toString() + "-" + (monthOfYear + 1) + "-" + year)
-
+                    // Format the selected date as a string and store it in itdate variable
                     itdate=(dayOfMonth.toString() + "-" + (monthOfYear + 1) + "-" + year).toString()
-
+                    // Store the formatted date string in dateit variable
                     dateit=itdate.toString()
 
 
@@ -97,99 +83,95 @@ class MainActivity : AppCompatActivity() {
                 day
             )
 
-            datePickerDialog.show()
+            datePickerDialog.show() // Show the DatePickerDialog
 
         }
-        dateit=date.text.toString()
+        dateit=date.text.toString() // Get the text from the date TextView and store it in dateit variable
 
-        val sdf = SimpleDateFormat("dd/M/yyyy")
-        val currentDate = sdf.format(Date())
-        date.setText(currentDate)
+        val sdf = SimpleDateFormat("dd/M/yyyy") // Create a SimpleDateFormat object with the specified date format
+        val currentDate = sdf.format(Date()) // Format the current date and store it in currentDate variable
+        date.setText(currentDate) // Set the text of the date TextView to the formatted current date
 
-        binding.button.setOnClickListener {
-            val int_name:String=input.text.toString()
-            val int_price:String=input2.text.toString()
+        binding.button.setOnClickListener { // Set a click listener for the button inside the binding
+            val int_name:String=input.text.toString() // Get the text from the input EditText and store it in int_name variable
+            val int_price:String=input2.text.toString() // Get the text from the input2 EditText and store it in int_price variable
 //            val k=listcat.get(position)
-            if(count>0)
+            if(count>0) // Check if count is greater than 0
             {
+                // If count is greater than 0, add a new listdata object with the input values and selected date to the listcat
                 listcat.add(listdata("$int_name","₹$int_price","$dateit"))
                 fileHelper.saveData(listcat)
 
             }
             else{
+                // If count is not greater than 0, add a new listdata object with the input values and current date to the listcat
                 listcat.add(listdata("$int_name","₹$int_price","$currentDate"))
-                fileHelper.saveData(listcat)
+                fileHelper.saveData(listcat) // Save the updated listcat to file
             }
-
+            // Clear the input fields and set the date TextView to the current date
             input.setText("")
             input2.setText("")
             date.setText(currentDate)
+            // Set the layout manager and adapter for the recyclerView1
             binding.recycleView1.layoutManager=LinearLayoutManager(this,LinearLayoutManager.VERTICAL,true)
             binding.recycleView1.adapter=list_adapter(this,listcat)
 
         }
+        // Set the layout manager and adapter for the recyclerView1
         binding.recycleView1.layoutManager=LinearLayoutManager(this,LinearLayoutManager.VERTICAL,true)
         binding.recycleView1.adapter=list_adapter(this,listcat)
 
 
 
-        delete1.setOnClickListener {
-            listcat.clear()
+        delete1.setOnClickListener { // Set a click listener for the delete1 button
+            listcat.clear() // Clear the listcat
+            // Set the layout manager and adapter for the recyclerView1
             binding.recycleView1.layoutManager=LinearLayoutManager(this,LinearLayoutManager.VERTICAL,true)
             binding.recycleView1.adapter=list_adapter(this,listcat)
-            fileHelper.saveData(listcat)
+            fileHelper.saveData(listcat) // Save the updated listcat to file
         }
 
 
-
-
-
-
-
-
+        // Set a query text listener for the searchView
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 return false
             }
 
             override fun onQueryTextChange(newText: String?): Boolean {
+                // Call filterList function with the new text
                 filterList(newText)
                 return true
             }
         })
 
 
-
-
-
+        // Set the overflow icon of the toolbar to the delete icon
         toolbar.overflowIcon=AppCompatResources.getDrawable(this,R.drawable.baseline_delete_24)
 
-
+        // Define a function to delete all items from the list
         fun deleteOn(){
-            listcat.clear()
+            listcat.clear() // Clear the listcat
+            // Set the layout manager and adapter for the recyclerView1
             binding.recycleView1.layoutManager=LinearLayoutManager(this,LinearLayoutManager.VERTICAL,true)
             binding.recycleView1.adapter=list_adapter(this,listcat)
-            fileHelper.saveData(listcat)
+            fileHelper.saveData(listcat) // Save the updated listcat to file
+            // Show a toast message indicating that all data are cleared
             Toast.makeText(this, "All data are cleared", Toast.LENGTH_SHORT).show()
         }
+        // Set a click listener for the toolbar menu items
         toolbar.setOnMenuItemClickListener{item ->
             when(item.itemId){
-                R.id.delete->deleteOn()
+                R.id.delete->deleteOn() // Call deleteOn function when delete menu item is clicked
             }
-            return@setOnMenuItemClickListener true
+            return@setOnMenuItemClickListener true // Return true to indicate that the menu item click event has been consumed
 
         }
-
-
-
 
 
     }
 
-
-
-
-
+    // Define a function to filter the list based on the given query
     fun filterList(query: String?) {
         val filteredList = ArrayList<listdata>()
 
@@ -207,8 +189,3 @@ class MainActivity : AppCompatActivity() {
         (binding.recycleView1.adapter as list_adapter).updateList(filteredList)
     }
     }
-
-
-
-
-
